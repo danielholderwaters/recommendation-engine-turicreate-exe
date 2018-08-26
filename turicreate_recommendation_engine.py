705d63e4b2d6998ee870cab87ea8705bbe92f383
Daniel Waters <42608070+danielholderwaters@users.noreply.github.com>
2018-08-26 15:28:27 -0400

    Update turicreate_recommendation_engine.py

--- turicreate_recommendation_engine.py
+++ turicreate_recommendation_engine.py
@@ -4,6 +4,10 @@
 
 #Requires installing turicreate, which requires using IOS or LINUX.  follow the instructions here:
 #https://github.com/apple/turicreate/blob/master/README.md
+# in the command prompt, this is helpful for installing packages once in the virtual environment::: sudo python -m pip install matplotlib
+
+#when in conEmu and have activated a virtual environment, paste the following into the command prompt to run:
+#python venv/turicreate_recommendation_engine.py
 
 #turicreate dependencies
 # pandas, decorator, prettytable, pillow, mxnet, coremltools, numpy, requests
@@ -38,7 +42,26 @@ r_cols = ['user_id', 'movie_id', 'rating', 'unix_timestamp']
 ratings_train = pd.read_csv('ml-100k/ua.base', sep='\t', names=r_cols, encoding='latin-1')
 ratings_test = pd.read_csv('ml-100k/ua.test', sep='\t', names=r_cols, encoding='latin-1')
 
+#After installing turicreate, 
+#first lets import it and read the train and test dataset in our environment. 
+#Since we will be using turicreate, we will need to convert the dataset in SFrames
+
 train_data = turicreate.SFrame(ratings_train)
 test_data = turicreate.SFrame(ratings_test)
-print(test_data)
+
+#starting with a popularity model - the movies that have the overall highest scores are at the top.
+popularity_model = turicreate.popularity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating')
+
+#this takes the popularity model and churns out a recommendation.
+popularity_recomm = popularity_model.recommend(users=[1,2,3,4,5],k=5)
+popularity_recomm.print_rows(num_rows=25)
+
+#we will now build a collaborative filtering model. Lets train the item similarity model and make top 5 recommendations for the first 5 users.
+#Training the model
+item_sim_model = turicreate.item_similarity_recommender.create(train_data, user_id='user_id', item_id='movie_id', target='rating', similarity_type='cosine')
+
+#Making recommendations
+item_sim_recomm = item_sim_model.recommend(users=[1,2,3,4,5],k=5)
+item_sim_recomm.print_rows(num_rows=25)
+
 print("Program is COMPLETE, mi sybiostro")
